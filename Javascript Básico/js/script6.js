b5557baa1d6fdba1c376ea34f87fc707ebd1f965
Daniel <danielcustodio_@outlook.com>
2021-03-16 18:24:02 -0300

    #28 - finished class about  arithmetic operations in JavaScript

---
+++ Javascript Básico/js/script6.js
@@ -0,0 +1,29 @@
+// + Adicao 
+// - Subtracao
+// * Multiplicacao
+// / Divisao
+// % Modulo
+// ++ Incremento
+// -- Decremento
+
+var a = 10;
+var b = 15;
+var c = a+b;
+var d = a-b;
+var e = a*b;
+var f = a/b;
+var g = b%a;
+var h = a++;
+var i = ++b;
+var j = --c;
+
+console.log(c);
+console.log(d);
+console.log(e);
+console.log(f);
+console.log(g);
+console.log(h);
+console.log(i);
+console.log(j);
+
+
