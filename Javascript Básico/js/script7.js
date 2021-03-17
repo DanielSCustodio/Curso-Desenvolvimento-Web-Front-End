3ba907e02f6a4f3edb28d18ba5375fd6fbed1075
Daniel <danielcustodio_@outlook.com>
2021-03-17 13:20:13 -0300

    #29 - Finished class about attribution - JavaScript

---
+++ Javascript Básico/js/script7.js
@@ -0,0 +1,18 @@
+var a = 100;
+var b = 100;
+var c = 100;
+var d = 100;
+var e = 102;
+
+a += 5;
+b -= 5;
+c *= 5;
+d /= 5;
+e %= 5;
+
+
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+console.log(e);
