5abc05f42a5463f589af8d1c420dcf4a97fdebbf
Daniel <danielcustodio_@outlook.com>
2021-03-17 14:07:37 -0300

    #30 - Finished class about comparative operators - JavaScript

---
+++ Javascript Básico/js/script8.js
@@ -0,0 +1,22 @@
+// = Igual a 
+// === valor igual e igual - Compara o valor e o tipo
+// != nao eh igual 
+// !== nao igual ou nao igual  - Compara o valor e o tipo
+// > maior que
+// < menor que 
+// >= maior que ou igual a 
+// <= menor ou igual a 
+
+var a = 10;
+var b = 12;
+var c = 10;
+var d = "12"
+
+console.log(a == b);
+console.log(b === d);
+console.log(a != c);
+console.log(b !== d);
+console.log(b > a);
+console.log(b < a);
+console.log(a >= b);
+console.log(a <= b);
