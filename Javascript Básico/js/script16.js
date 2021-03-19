5e6d533ffc80e9c01b18b1ff52d4d1745ddf8e4f
Daniel <danielcustodio_@outlook.com>
2021-03-19 12:35:51 -0300

    #37 - Finished class about function 2 - JavaScript

---
+++ Javascript Básico/js/script16.js
@@ -0,0 +1,16 @@
+function media (n1, n2){
+    media =(n1 + n2)/2;
+    return media;
+} 
+var result = media;
+var print = result(8,5);
+
+console.log(print);
+
+
+var calculo = function(n1, n2){
+    result = n1+ n2;
+    return result;
+}
+
+console.log(calculo(10,20));
