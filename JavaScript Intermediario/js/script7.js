6bafa903faa7a0dc017cd887a318e2450b1d683f
Daniel <danielcustodio_@outlook.com>
2021-03-20 15:53:03 -0300

    #49 - Finished class about events-part1 - JavaScript

---
+++ JavaScript Intermediario/js/script7.js
@@ -0,0 +1,16 @@
+var h1 = document.getElementsByTagName("h1")[0];
+
+function changeH1(input){
+    
+  h1.innerText = input.value;
+    
+}
+
+function hideH1(){
+  if(h1.style.display!="none"){
+    h1.style.display ="none";
+  }else{
+    h1.style.display ="block";
+  }
+}
+
