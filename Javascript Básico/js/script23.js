28b1880cdc563f3686301c451d8335237e289e11
Daniel <danielcustodio_@outlook.com>
2021-03-19 22:56:01 -0300

    #44 - Finished class about let, const, var

---
+++ Javascript Básico/js/script23.js
@@ -0,0 +1,22 @@
+var   n1 = 1;
+let   n2 = 2;
+const n3 = 3;
+const n4 = {}
+
+n4.fix=68;
+
+
+// n3 =65;
+
+console.log(n1);
+console.log(n2);
+console.log(n3);
+console.log(n4);
+
+
+function teste(){
+    let n5 =10;
+    return n1;
+}
+
+// console.log(n5); so funciona no escopo da funcao
